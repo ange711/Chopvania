76abed57af515bfbdc00694b5db60a9e43207d25
ange <angela.carriero@hotmail.com>
2014-10-25 15:20:59 -0400

    Updated aisles

--- Chopvania/Assets/Scripts/MainCamera.cs
+++ Chopvania/Assets/Scripts/MainCamera.cs
@@ -5,11 +5,22 @@ public class MainCamera : MonoBehaviour
 {
 
 	public Transform focus;
+	public float dampTime = 0.1f;
+	private Vector3 velocity = Vector3.zero;
 
-	void Update()
-	{
+	void Update(){
+
+//		Vector3 point = camera.WorldToViewportPoint(focus.position);
+//		Vector3 delta = focus.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+//		Vector3 destination = transform.position + delta;
+//		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
 		Vector3 nextPos = transform.position;
 		nextPos.x = focus.position.x + 4;
-		transform.position = nextPos;
+		float ydiff = Mathf.Abs(focus.position.y - nextPos.y);
+		if(ydiff > 3){
+			nextPos.y = focus.position.y;
+		}
+		transform.position = Vector3.SmoothDamp(transform.position, nextPos, ref velocity, dampTime);
 	}
 }
